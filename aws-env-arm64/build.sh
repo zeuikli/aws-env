8d116db49cc252c6981f47eda5a5497b16726de2
Zeuik Li <aids4002004@gmail.com>
2022-09-06 16:34:37 +0800

    Update arm packages and Go Library

---
+++ aws-env-arm64/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BUILD_DIR=bin
+NAME=aws-env
+
+mkdir $BUILD_DIR
+
+for GOOS in linux; do
+    for GOARCH in arm64 amd64; do
+        GOOS=$GOOS GOARCH=$GOARCH go build -v -o $BUILD_DIR/$NAME-$GOOS-$GOARCH
+    done
+done
---
+++ aws-env-arm64/go.mod
@@ -0,0 +1,7 @@
+module aws-env
+
+go 1.18
+
+require github.com/aws/aws-sdk-go v1.44.86
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
